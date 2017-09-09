3ac0ce2c54444f8f210cd4e1e77ecd8778f93a44
Sa2Knight <shingo.sasaki.0529@gmail.com>
2017-09-09 17:43:15 +0000

    足し算を学習するスクリプトを実装

---
+++ python/ml/addition.py
@@ -0,0 +1,36 @@
+#
+# ２つの整数の足し算を機械学習し、任意の足し算を回答させる
+#
+import numpy as np
+import matplotlib.pyplot as plt
+from pandas import DataFrame
+from sklearn import linear_model, datasets
+
+# 足し算の例とその答えを用意する
+formulas = DataFrame([
+    [0, 0],
+    [0, 1],
+    [0, 2],
+    [1, 0],
+    [1, 1],
+    [1, 2],
+    [2, 0],
+    [2, 1],
+    [2, 2]
+])
+answers = DataFrame([0, 1, 2, 1, 2, 3, 2, 3, 4])
+
+# 式と答えを線形回帰学習させる
+model = linear_model.LinearRegression()
+model.fit(formulas, answers)
+print('学習完了')
+
+while True:
+
+    # 標準入力から計算式を取得
+    print('> ', end='')
+    x, y = list(map(lambda x: int(x), input().split(' ')))
+
+    # 学習モデルを用いて回答を取得し、標準出力
+    predected_answer = model.predict([[x, y]])
+    print("{0} + {1} = {2}".format(x, y, int(predected_answer[0][0])))
