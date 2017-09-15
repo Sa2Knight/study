380c81b650e0274bb76abc1cb1eb123e97c39fdd
Sa2Knight <shingo.sasaki.0529@gmail.com>
2017-09-15 13:25:00 +0000

    車の予測

--- python/ml/cars.py
+++ python/ml/cars.py
@@ -1,11 +1,10 @@
 #
-# ２つの整数の足し算を機械学習し、任意の足し算を回答させる
-#
 # 車の製品情報と価格を学習し、価格を予測させる
 #
 import pandas as pd
-from sklearn import linear_model, metrics
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import preprocessing
 
 # CSVファイルを読み込む
 df = pd.read_csv('cars.csv')
@@ -19,9 +18,24 @@ for col in ['num-of-doors', 'price', 'bore', 'stroke', 'horsepower', 'peak-rpm']
 label = df['price']
 data  = df.drop('price', axis=1)
 
+# 文字列データを数値化
+le = preprocessing.LabelEncoder()
+for col in ['make', 'fuel-type', 'aspiration', 'num-of-doors', 'body-style', 'drive-wheels', 'engine-location', 'engine-type', 'num-of-cylinders', 'fuel-system']:
+  data[col] = le.fit_transform(data[col])
+
 # 学習用データとテストデータにわける
 train_data, test_data, train_label, test_label = train_test_split(data, label)
 
 # 学習する
+clf = RandomForestClassifier()
+clf.fit(train_data, train_label)
 
+# 評価する
+predict = clf.predict(test_data)
+rate_sum = 0
 
+for i in range(len(test_label)):
+  t = int(test_label.iloc[i])
+  p = int(predict[i])
+  rate_sum += int(min(t, p) / max(t, p) * 100)
+print(rate_sum / len(test_label))
