a3c979fca714c714368cdd609805d6cd679f957d
Sa2Knight <shingo.sasaki.0529@gmail.com>
2017-10-28 21:18:13 +0900

    GoogleアナリティクスAPIのハローワールド

---
+++ ruby/analitics/Gemfile
@@ -0,0 +1,3 @@
+# frozen_string_literal: true
+source "https://rubygems.org"
+gem 'google-api-client', '~> 0.9'
---
+++ ruby/analitics/main.rb
@@ -0,0 +1,38 @@
+require 'google/apis/analyticsreporting_v4'
+require 'pp'
+
+# GoogleアナリティクスAPIを利用するためのライブラリ
+analytics = Google::Apis::AnalyticsreportingV4
+
+# GoogleアナリティクスAPIの認証先URL
+scope = ['https://www.googleapis.com/auth/analytics.readonly']
+
+# GoogleアナリティクスAPIの認証を受ける
+client = analytics::AnalyticsReportingService.new
+client.authorization = Google::Auth::ServiceAccountCredentials.make_creds(
+  json_key_io: File.open('auth.json'),
+  scope: scope
+)
+
+# 対象のアナリティクスのビューID
+view_id = '158527891'
+
+# レポートの取得期間を直近７日間に
+date_range = analytics::DateRange.new(start_date: '7DaysAgo', end_date: 'today')
+
+# レポートの対象をセッション単位に
+metric = analytics::Metric.new(expression: 'ga:sessions', alias: 'sessions')
+
+# ディメンションの設定(?)
+dimension = analytics::Dimension.new(name: 'ga:browser')
+
+# GoogleアナリティクスAPIより、レポートの取得
+request = analytics::GetReportsRequest.new(
+  report_requests: [analytics::ReportRequest.new(
+    view_id: view_id, metrics: [metric], dimensions: [dimension], date_ranges: [date_range]
+  )]
+)
+response = client.batch_get_reports(request)
+
+# 結果の出力
+pp response.reports
