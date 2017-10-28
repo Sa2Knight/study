d4a3000d33c79742e198709394ffdd82cc890c4d
Sa2Knight <shingo.sasaki.0529@gmail.com>
2017-10-28 23:10:16 +0900

    その日のページ別ビュー数を出力するように

--- ruby/analitics/main.rb
+++ ruby/analitics/main.rb
@@ -1,6 +1,9 @@
 require 'google/apis/analyticsreporting_v4'
 require 'pp'
 
+# 対象サイトのURL
+BASE_URL = "http://qs.nndo.jp"
+
 # GoogleアナリティクスAPIを利用するためのライブラリ
 analytics = Google::Apis::AnalyticsreportingV4
 
@@ -17,17 +20,33 @@ client.authorization = Google::Auth::ServiceAccountCredentials.make_creds(
 # 対象のアナリティクスのビューID
 view_id = '158527891'
 
+# 集計期間を直近本日に
+date_range = analytics::DateRange.new(start_date: 'today', end_date: 'today')
+
 # レポートの対象をセッション単位に
 metric = analytics::Metric.new(expression: 'ga:users', alias: 'users')
+
+# ページ別に集計する
+dimension = analytics::Dimension.new(name: 'ga:pagePath')
+
+# カウント数順にソート
+order_by = analytics::OrderBy.new(field_name: 'ga:users', sort_order: 'DESCENDING')
 
 # GoogleアナリティクスAPIより、レポートの取得
 request = analytics::GetReportsRequest.new(
   report_requests: [analytics::ReportRequest.new(
     view_id: view_id,
     metrics: [metric],
+    date_ranges: [date_range],
+    dimensions: [dimension],
+    order_bys: [order_by],
   )]
 )
 response = client.batch_get_reports(request)
 
 # 結果の出力
-pp response.reports.first.data.totals.first.values.first
+data = response.reports.first.data
+puts "累計ユーザ数: #{data.totals.first.values.first}"
+data.rows.each do |row|
+  puts "#{row.metrics.first.values.first}: #{BASE_URL + row.dimensions.first}"
+end
